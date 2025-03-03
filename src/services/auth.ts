798f2236fa7b7b414f82394319233a371e7320c0
Pratim Sadhu <pratimsadhu@icloud.com>
2025-03-02 20:01:52 -0500

    auth service skeleton

---
+++ src/services/auth.ts
@@ -0,0 +1,8 @@
+import supabaseClient from '@config/supabase';
+
+const authService = {
+	signUp: async (email: string, password: string) => {},
+	signIn: async (email: string, password: string) => {},
+};
+
+export default authService;
