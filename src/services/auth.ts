187fc41be3aa0be66bc09b0ba955f8cb819fd6e7
Pratim Sadhu <pratimsadhu@icloud.com>
2025-03-02 20:57:24 -0500

    check for duplicate user

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,7 +4,7 @@ import { hashPassword, verifyPassword } from '@utils/argon';
 
 const authService = {
 	/**
-	 * Signs up a user
+	 * Signs up a user ensuring unique (email, clientId) pairs
 	 * @param email The email of the user to sign up
 	 * @param password The password of the user to sign up
 	 * @param clientId The unique ID of website the user is signing up for
@@ -12,12 +12,40 @@ const authService = {
 	 */
 	signUp: async (email: string, password: string, clientId: string) => {
 		try {
+			// Check if the user with this email & clientId already exists
+			const { data: existingUser, error: findError } = await supabaseClient
+				.from('users')
+				.select('id')
+				.eq('email', email)
+				.eq('client_id', clientId)
+				.maybeSingle();
+
+			if (findError) {
+				throw new Error(findError.message);
+			}
+
+			// If user exists, return error
+			if (existingUser) {
+				return {
+					error: 'User with this email already exists for this client.',
+					status: 409,
+				};
+			}
+
+			// Hash the password
 			const hashedPassword = await hashPassword(password);
-			const { error } = await supabaseClient.from('users').upsert({
-				email: email,
-				password: hashedPassword,
-				client_id: clientId,
-			});
+
+			const { data, error } = await supabaseClient
+				.from('users')
+				.upsert([
+					{
+						email: email,
+						password: hashedPassword,
+						client_id: clientId,
+					},
+				])
+				.select('*')
+				.single();
 
 			if (error) {
 				throw new Error(error.message);
@@ -42,17 +70,18 @@ const authService = {
 				.from('users')
 				.select('*')
 				.eq('email', email)
-				.eq('client_id', clientId);
+				.eq('client_id', clientId)
+				.maybeSingle();
 
 			if (error) {
 				throw new Error(error.message);
 			}
 
-			if (!data || data.length === 0) {
+			if (!data) {
 				return { error: 'User not found', status: 404 };
 			}
 
-			const user = data[0];
+			const user = data;
 			const isPasswordValid = await verifyPassword(user.password, password);
 
 			if (!isPasswordValid) {
