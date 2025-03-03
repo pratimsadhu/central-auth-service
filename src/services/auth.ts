2b1d69c892953b5615205fe455f53abe5989401d
Pratim Sadhu <pratimsadhu@icloud.com>
2025-03-02 20:44:11 -0500

    signin fn

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,6 @@
 import supabaseClient from '@config/supabase';
-import { hashPassword } from '@utils/argon';
+import { generateJwtToken } from '@utils/jwt';
+import { hashPassword, verifyPassword } from '@utils/argon';
 
 const authService = {
 	/**
@@ -11,7 +12,7 @@ const authService = {
 	 */
 	signUp: async (email: string, password: string, clientId: string) => {
 		try {
-			const hashedPassword = hashPassword(password);
+			const hashedPassword = await hashPassword(password);
 			const { error } = await supabaseClient.from('users').upsert({
 				email: email,
 				password: hashedPassword,
@@ -23,11 +24,63 @@ const authService = {
 			}
 
 			return { message: 'Sign up successful', status: 200 };
-		} catch (error) {
-			return { error: error, status: 500 };
+		} catch (error: any) {
+			return { error: error.message || 'Internal Server Error', status: 500 };
 		}
 	},
-	signIn: async (email: string, password: string, clientId: string) => {},
+
+	/**
+	 * Signs in a user
+	 * @param email The email of the user to sign in
+	 * @param password The password of the user to sign in
+	 * @param clientId The unique ID of website the user is signing in for
+	 * @returns The token, refresh token, message, and status code
+	 */
+	signIn: async (email: string, password: string, clientId: string) => {
+		try {
+			const { data, error } = await supabaseClient
+				.from('users')
+				.select('*')
+				.eq('email', email)
+				.eq('client_id', clientId);
+
+			if (error) {
+				throw new Error(error.message);
+			}
+
+			if (!data || data.length === 0) {
+				return { error: 'User not found', status: 404 };
+			}
+
+			const user = data[0];
+			const isPasswordValid = await verifyPassword(user.password, password);
+
+			if (!isPasswordValid) {
+				return { error: 'Invalid password', status: 401 };
+			}
+
+			const payload = {
+				user_id: user.id,
+				email: email,
+				client_id: clientId,
+			};
+
+			const token = generateJwtToken(payload, 60 * 60);
+			const refreshToken = generateJwtToken(
+				{ ...payload, access_token: token },
+				30 * 24 * 60 * 60
+			);
+
+			return {
+				token,
+				refreshToken,
+				message: 'Sign in successful',
+				status: 200,
+			};
+		} catch (error: any) {
+			return { error: error.message || 'Internal Server Error', status: 500 };
+		}
+	},
 };
 
 export default authService;
