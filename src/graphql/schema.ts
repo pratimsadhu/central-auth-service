ad7069fa557529d5962bbd67c345ca0bed5092a6
Pratim Sadhu <pratimsadhu@icloud.com>
2025-03-04 13:58:47 -0500

    fix schema to include all resolvers

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -11,13 +11,26 @@ const typeDefs = gql`
 		user: User!
 	}
 
+	input UserUpdateInput {
+		email: String
+		password: String
+	}
+
 	type Query {
 		verifyToken(token: String!): User
+		generateRefreshToken(token: String!): String
 	}
 
 	type Mutation {
-		signUp(email: String!, password: String!): AuthPayload!
-		signIn(email: String!, password: String!): AuthPayload!
+		signUp(email: String!, password: String!, client_id: String!): AuthPayload!
+		signIn(email: String!, password: String!, client_id: String!): AuthPayload!
+		updateUser(
+			user_id: String!
+			client_id: String!
+			token: String!
+			updated_data: UserUpdateInput!
+		): AuthPayload!
+		deleteUser(user_id: String!, client_id: String!, token: String!): Boolean!
 	}
 `;
 
