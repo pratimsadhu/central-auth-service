486ba44ecd092ee09637e5d40cbf1cc3c1f9e0b2
Pratim Sadhu <pratimsadhu@icloud.com>
2025-03-04 13:57:50 -0500

    create apollo server

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from '@graphql/schema';
+import resolvers from '@graphql/resolvers';
 
 // Load environment variables
 dotenv.config();
@@ -12,25 +15,37 @@ const PORT = process.env.PORT || 8001;
 
 // Middleware
 const corsOptions = {
-	origin: '*', // Allow all origins (modify as needed)
+	origin: '*', // TODO: Allow specific domains
 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-const logger = morgan('dev');
-
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(helmet());
-app.use(logger);
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-	res.json({
-		message: `Central Auth Service with Supabase is running in port!`,
+async function startApolloServer() {
+	const server = new ApolloServer({ typeDefs, resolvers });
+
+	await server.start();
+	server.applyMiddleware({ app } as any);
+
+	app.get('/', (req, res) => {
+		res.json({
+			message: `Central Auth Service with Supabase is running in port!`,
+		});
 	});
-});
 
-// Start Server
-app.listen(PORT, () => {
-	console.log(`Server running at http://localhost:${PORT}`);
-});
+	// Start Server
+	app.listen(PORT, () => {
+		console.log(`Server running at http://localhost:${PORT}`);
+		console.log(
+			`GraphQL endpoint available at http://localhost:${PORT}${server.graphqlPath}`
+		);
+	});
+}
+
+startApolloServer().catch((err) =>
+	console.error('Failed to start server:', err)
+);
